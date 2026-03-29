d761c468af93124fe17465bce5cd1b1417e7e61d
efp-bsc-com-02-22-png <efp-bsc-com-02-22@unima.ac.mw>
2026-03-29 13:43:48 +0100

    update changes

--- studentManagementSystem.cpp
+++ studentManagementSystem.cpp
@@ -5,36 +5,56 @@
 
 using namespace std;
 
-int main() {
+int main()
+{
     Student s;
 
-    //get name of the student
+    // get name of the student
     string nameOfStudent;
     cout << "Please enter name of the student: " << endl;
-    cin >> nameOfStudent; //get name of the student 
+    cin >> nameOfStudent; // get name of the student
     s.SetName(nameOfStudent);
 
-    //get age of the student 
+    // get age of the student
     int ageOfStudent;
     cout << "How old is " << nameOfStudent << "?" << endl;
-    cin >> ageOfStudent;//get age of the student 
+    cin >> ageOfStudent; // get age of the student
     s.SetAge(ageOfStudent);
 
-    //get GPA of the student
+    // get GPA of the student
     float gpaofStudent;
     cout << "Enter GPA: ";
-    cin >> gpaofStudent;//get GPA of the student 
+    cin >> gpaofStudent; // get GPA of the student
     s.SetGPA(gpaofStudent);
 
-    //create a  file to store the student details.
-    ofstream file("student_infor.txt");
-    file << "Name of Student: " <<nameOfStudent << endl;
-    file << "Age of Student: " << ageOfStudent << endl;
-    file << "GPA of Student: " << ;
+    // get enrollment status of the student
+    string enrollmentStatus;
+    cout << "Is the student enrolled? (yes or no): ";
+    cin >> enrollmentStatus; // get enrollment status of the student
+    bool isEnrolled;
+    if (enrollmentStatus == "yes")
+    {
+        isEnrolled = true;
+        cout << "Student enrolled successfully." << endl;
+    }
+    else if (enrollmentStatus == "no")
+    {
+        isEnrolled = false;
+    }
+    else
+    {
+        cout << "Invalid input for enrollment status. Please enter 'yes' or 'no'." << endl;
+    }
+    s.SetEnrolled(isEnrolled);
 
-    //close the file
+    // create a  file to store the student details.
+    ofstream file("student_infor.txt");
+    file << "Name of Student: " << nameOfStudent << endl;
+    file << "Age of Student: " << ageOfStudent << endl;
+    file << "GPA of Student: " << gpaofStudent << endl;
+
+    // close the file
     file.close();
 
     return 0;
 }
-
